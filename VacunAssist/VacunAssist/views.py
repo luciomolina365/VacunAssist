c00c7efdde8f7e93f87d2fcc215bb34a7152bf71
Tobi Ajenjo <57407963+Maycol29@users.noreply.github.com>
2022-05-10 00:47:20 -0300

    agregando los primeros render

--- VacunAssist/VacunAssist/views.py
+++ VacunAssist/VacunAssist/views.py
@@ -1,4 +1,10 @@
+from cgitb import html
+from pipes import Template
 from django.http import HttpResponse
+from django.template import Context, Template
+from django.shortcuts import render
+
 
 def saludo(request):
-    return HttpResponse("HOLA")
+    return render(request , 'prueba.html')
+
