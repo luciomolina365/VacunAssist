d2bf94b86056fb54209d39ee86db13e38172acbd
luciomolina365 <lucio_molina_365@live.com>
2022-05-13 16:20:23 -0300

    Models

--- VacunAssist/gestionVacunatorio/models.py
+++ VacunAssist/gestionVacunatorio/models.py
@@ -3,17 +3,33 @@ from django.db import models
 
 # Create your models here.
 class Client(models.Model):
+
+    zones = [
+        ("T","Terminal de ómnibus"), 
+        ("M","Municipalidad de La Plata"),
+        ("C","Cementerio")
+    ]
+
+    genres = [
+        ("M","Masculino"),
+        ("F","Femenino"),
+        ("O","Otro")
+    ]
+
     name=models.CharField(max_length=30)
     surname=models.CharField(max_length=30)
     dni=models.IntegerField()
     dateOfBirth=models.DateField()
-    zone=models.CharField(max_length=30)
+    zone=models.CharField(max_length=30 , choices = zones)
     email=models.CharField(max_length=30)
     password=models.CharField(max_length=30)
-    SecondFactor=models.IntegerField()
+    secondFactor=models.IntegerField()
+    genre=models.CharField(max_length=30 , choices = genres)
 
 class Vaccinator(models.Model):
     name=models.CharField(max_length=30)
     surname=models.CharField(max_length=30)
     dni=models.IntegerField()
     email=models.CharField(max_length=30)
+
+
