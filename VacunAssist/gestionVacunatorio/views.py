108319d350e3fe9fb6767af6288813ee8038dddd
luciomolina365 <lucio_molina_365@live.com>
2022-05-16 00:19:40 -0300

    UserRegistration

--- VacunAssist/gestionVacunatorio/views.py
+++ VacunAssist/gestionVacunatorio/views.py
@@ -2,8 +2,10 @@ import argparse
 from ast import If
 from inspect import ArgSpec
 from django.shortcuts import render, HttpResponse
+from django.views.generic import View
 from gestionVacunatorio.models import Client
-from gestionVacunatorio.forms import RegistrationForm
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 
 
@@ -15,32 +17,15 @@ def home(request):
     return render(request,'home.html')
 
 
-def user_registration(request):
-    
+class UserRegistration(View):
 
-    if  not request.method == "POST":  #sacar NOT
-        form = RegistrationForm(request.POST)
-
-        if form.is_valid():
-
-            return render(request,"login.html") ##login no existe todavia
-    else:
-        form = RegistrationForm()
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "registration/user_registration.html", {"form": form})
 
 
-    return render(request, "user_registration.html", {"form":form})
-
-    """response = HttpResponse()
-    response["name"] = request.get["name"]
-    response["surname"] = request.get["surname"]
-    response["dni"] = request.get["dni"]
-    response["dateOfBirth"] = request.get["dateOfBirth"]
-    response["zone"] = request.get["zone"]
-    response["email"] = request.get["email"]
-    response["password"] = request.get["password"]
-    response["password_confirm"] = request.get["password_confirm"]
-    response["secondFactor"] = request.get["secondFactor"]"""
-
+    def post(self, request):
+        pass
 
     
 
