9765724f9e405fd8c5ff81cd09e2dfaceee87010
Tobi Ajenjo <57407963+Maycol29@users.noreply.github.com>
2022-05-16 22:21:15 -0300

    Agregacion de forms y apartados para el admin

--- VacunAssist/gestionVacunatorio/forms.py
+++ VacunAssist/gestionVacunatorio/forms.py
@@ -1,8 +1,26 @@
 from dataclasses import fields
 from django.forms import ModelForm
-from .models import User
+from .models import *
+
 
 class UserRegForm(ModelForm):
     class Meta: 
         model = User
         fields = ('name','surname','dni','dateOfBirth','zone','email','password','gender')
+
+class VaccinatorRegForm(ModelForm):
+    class Meta: 
+        model = Vaccinator
+        fields = ('name','surname','dni','email','password')
+
+class FormularyRegForm(ModelForm):
+    class Meta: 
+        model = Formulary
+        #falta user
+        fields = ('risk','admissionDate')
+
+class ForumRegForm(ModelForm):
+    class Meta: 
+        model = Forum
+        fields = ('title','description',"date")
+        
